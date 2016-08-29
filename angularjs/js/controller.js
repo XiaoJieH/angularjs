c52c3f321a1ffeb0b1bacff0088369c9704f8ad4
[hexiaojie] <[915439014@qq.com]>
2016-08-29 17:17:36 +0800

    orderby

--- angularjs/js/controller.js
+++ angularjs/js/controller.js
@@ -1,11 +1,15 @@
 angular.module('myApp', []).controller('PhoneListCtrl', function($scope) {
   $scope.phones = [
-    {"name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S."},
+   {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S.",
+     "age": 3},
     {"name": "Motorola XOOM™ with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet."},
+     "snippet": "The Next, Next Generation tablet.",
+     "age": 1},
     {"name": "MOTOROLA XOOM™",
-     "snippet": "The Next, Next Generation tablet."}
+     "snippet": "The Next, Next Generation tablet.",
+     "age": 2}
   ];
+   $scope.order = 'name';
     $scope.hello = "Hello, World!"
 });
